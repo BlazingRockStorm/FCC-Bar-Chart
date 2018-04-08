265ab8736e6ad6c1046c1d445898a95af54e5b0c
BlazingRockStorm <rocker.gryphon@gmail.com>
2018-04-08 23:20:52 +0700

    api readable

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import * as d3 from 'd3';
-
-var formatCurrency = d3.format("$,.2f");
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+import axios from 'axios';
+// import * as d3 from 'd3';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      description: ''
+      description: '',
+      data: []
     };
   }
 
-  componentWillMount() {
-    d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", response => {
-      this.setState({ description: response.description });
-    });
+  componentDidMount() {
+    axios.get("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json").then(response => {
+      let api = response.data ;
+      this.setState({ 
+        description: api.description,
+        data: api.data[1][0] 
+      });
+      console.log(api.data[1]);
+    })
+    .catch(err => console.log(err));
   }
 
   render() {
@@ -23,6 +28,7 @@ class App extends Component {
     <div className="container">  
       <div className="header">Gross Domestic Product</div>
       <svg className="chart"></svg>
+      <div>{this.state.data}</div>
       <div className="notes">{this.state.description}</div>
       <div className="tooltip"><b>Header</b><br /><span>Val</span></div>
     </div>
